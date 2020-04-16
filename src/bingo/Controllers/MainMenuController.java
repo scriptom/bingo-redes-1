d996b8ef695f9cba87290610cc3a8b66c5015d0f
robegon98 <ragonzalez1899@gmail.com>
2020-04-15 21:02:41 -0400

    Creacion de los cartones de bingo en partida y algunos cambios de css
    lo del titulo xd

--- src/bingo/Controllers/MainMenuController.java
+++ src/bingo/Controllers/MainMenuController.java
@@ -7,7 +7,20 @@ package bingo.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +36,18 @@ public class MainMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+    public GridPane llenar(GridPane panel) {
 
+        return panel;
+    }
+    @FXML
+    private void goToPartida(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/bingo/vistas/Partida.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/bingo/vistas/MyStyles.css");
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.setTitle("Lobby");
+        window.show();
+    }
 }
